05fde8b4a9666c69a6e41181190d920195327ed4
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-08-28 18:38:52 -0300

    Add carregamento na tela inicial

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:family_home/assets/button_styles.dart';
 
+//CLASSE DA TELA DE LOGIN
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -9,104 +10,100 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool _isObscured = true; // Para controlar a visibilidade da senha
+  //VISIBILIDADE DA SENHA
+  bool _isObscured = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF577096), // Cor do topo da tela
-      body: Stack(
-        children: [
-          // Parte inferior com borda arredondada
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height *
-                  0.7, // Altura da parte arredondada
-              decoration: const BoxDecoration(
-                color: Color(0xFFA8BEE0), // Cor de fundo da parte inferior
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.elliptical(400, 200),
-                  topRight: Radius.elliptical(150, 10),
+      //COR TOPO DA TELA
+      backgroundColor: const Color(0xFF577096),
+      //SAFEAREA E SINGLECHILDSCROLLVIEW: GARANTE QUE O CONTEÚDO DA TELA SEJA ROLÁVEL E NÃO SUBA QUANDO O TECLADO APARECER
+      body: SafeArea(
+        child: SingleChildScrollView(
+          //COLUMN: AJUSTA OS WIDGETS PARA DEIXAR-LOS DE FORMA LINEAR, SENDO MELHOR PARA QUANDO O CONTEÚDO POSSA SER ROLADO
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70, bottom: 10),
+                child: Image.asset(
+                  'lib/assets/icons/logo.png',
+                  height: 150,
+                  width: 150,
+                  fit: BoxFit.cover,
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Texto de boas-vindas
-                    const Text(
-                      'Bem vindo!',
-                      style: TextStyle(fontSize: 24, color: Color(0xFF2B3649)),
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Campo de texto para Nome
-                    TextField(
-                      decoration: ButtonStyles.textFieldDecoration.copyWith(
-                        labelText: 'Your name',
-                        prefixIcon: const Icon(Icons.person),
+              //BORDA ARREDONDADA
+              Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: const BoxDecoration(
+                  //COR FUNDO PARTE INFERIOR
+                  color: Color(0xFFA8BEE0),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.elliptical(400, 200),
+                    topRight: Radius.elliptical(150, 10),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    //ALINHAR AO CENTRO
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Bem vindo!',
+                        style:
+                            TextStyle(fontSize: 24, color: Color(0xFF2B3649)),
                       ),
-                    ),
-                    const SizedBox(height: 16),
+                      const SizedBox(height: 20),
 
-                    // Campo de texto para Código da Família
-                    TextField(
-                      decoration: ButtonStyles.textFieldDecoration.copyWith(
-                        labelText: 'Family Code',
-                        prefixIcon: const Icon(Icons.qr_code_2),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-
-                    // Campo de texto para Senha
-                    TextField(
-                      obscureText: _isObscured,
-                      decoration: ButtonStyles.textFieldDecoration.copyWith(
-                        labelText: 'Password',
-                        prefixIcon: const Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isObscured
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isObscured = !_isObscured;
-                            });
-                          },
+                      //CAMPO DE TEXTO INSERIR NOME
+                      TextField(
+                        decoration: ButtonStyles.textFieldDecoration.copyWith(
+                          labelText: 'Your name',
+                          prefixIcon: const Icon(Icons.person),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
+                      const SizedBox(height: 16),
+
+                      //CAMPO DE TEXTO INSERIR NOME CODIGO FAMÍLIA
+                      TextField(
+                        decoration: ButtonStyles.textFieldDecoration.copyWith(
+                          labelText: 'Family Code',
+                          prefixIcon: const Icon(Icons.qr_code_2),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+
+                      //CAMPO DE TEXTO INSERIR SENHA
+                      TextField(
+                        obscureText: _isObscured,
+                        decoration: ButtonStyles.textFieldDecoration.copyWith(
+                          labelText: 'Password',
+                          prefixIcon: const Icon(Icons.lock),
+                          //VISUALIZAR SENHA
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isObscured
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isObscured = !_isObscured;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-
-          // Logo no centro da tela
-          Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 70, bottom: 10),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  // Imagem da logo
-                  Image.asset(
-                    'lib/assets/icons/logo.png',
-                    height: 150,
-                    width: 150,
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-//IMPORTANDO TELAS
+
+// IMPORTANDO TELAS
 import 'login/login.dart';
 
-//IMPORTANDO STYLES DOS BOTÕES
-import 'assets/button_styles.dart';
+//IMPORT PARA UTILIZAR O TIMER
+import 'dart:async'; 
 
 void main() {
   runApp(const MyApp());
@@ -16,39 +18,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ROTA PRINCIPAL QUANDO INICIALIZA O APP
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeScreen(),
-        '/loginScreen': (context) => const LoginScreen(),
+        // ROTAS DO APP
+        '/': (context) => const SplashScreen(), 
+        '/loginScreen': (context) => const LoginScreen(), 
       },
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+// TELA DE CARREGAMENTO (Splash Screen)
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //INICIA TEMPORIZADOR DE 3 SEGUNDOS PARA ABRIR O APP
+    Future.delayed(const Duration(seconds: 3), () {
+      // APÓS OS 3 SEGUNDOS, REDIRECIONA PARA A TELA DE LOGIN
+      Navigator.pushReplacementNamed(context, '/loginScreen');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFA8BEE0),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF577096),
-        title: const Text(
-          'Family Home',
-          style: TextStyle(
-            color: Color(0xFFA8BEE0),
-          ),
-        ),
-      ),
+    return const Scaffold(
+      //COR DE FUNDO DO APP
+      backgroundColor: Color(0xFFA8BEE0),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/loginScreen');
-          },
-          //COLOCANDO O STYLE BUTTON
-          style: ButtonStyles.primaryButton,
-          child: const Text('Tela de Login'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //ANIMAÇÃO DE CARREGAMENTO
+            SpinKitCircle(
+              //COR PRINCIPAL DO APP
+              color: Color(0xFF577096),
+              //TAMANHO DO CÍRCULOA
+              size: 100.0, 
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                fontSize: 18,
+                color: Color(0xFF2B3649),
+              ),
+            ),
+          ],
         ),
       ),
     );
