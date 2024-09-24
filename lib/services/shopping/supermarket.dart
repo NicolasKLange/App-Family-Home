a047991a4f5395aa02f0971adc9149a2f37bd4f0
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-09-24 18:11:20 -0300

    adicionando lista

--- lib/services/shopping/supermarket.dart
+++ lib/services/shopping/supermarket.dart
@@ -10,6 +10,50 @@ class SupermarketScreen extends StatefulWidget {
 }
 
 class _SupermarketScreenState extends State<SupermarketScreen> {
+  List<Map<String, dynamic>> _shoppingList = [
+    // Lista inicial de exemplo
+    {'item': '2 pacote de macarrão', 'checked': false},
+    {'item': 'arroz', 'checked': false},
+  ];
+
+  // Função para adicionar novo item
+  void _addItem(String newItem) {
+    setState(() {
+      _shoppingList.add({'item': newItem, 'checked': false});
+    });
+  }
+
+  // Mostra um diálogo para adicionar um novo item
+  void _showAddItemDialog() {
+    TextEditingController itemController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Adicionar Item"),
+          content: TextField(
+            controller: itemController,
+            decoration: const InputDecoration(
+              hintText: "Digite o nome do item",
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                if (itemController.text.isNotEmpty) {
+                  _addItem(itemController.text);
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text("Adicionar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +63,7 @@ class _SupermarketScreenState extends State<SupermarketScreen> {
           imagePath: 'lib/assets/icons/logo.png',
           child: Column(
             children: [
-              //ROW PARA COLOCAR O BOTAO DE VOLTAR
+              //ROW PARA COLOCAR O BOTÃO DE VOLTAR E O TÍTULO
               Row(
                 children: [
                   const SizedBox(
@@ -42,7 +86,68 @@ class _SupermarketScreenState extends State<SupermarketScreen> {
                   ),
                 ],
               ),
-              // Conteúdo da tela de tarefas aqui...
+              // LISTA DE COMPRAS
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: const Color(0xFF2B3649),
+                    width: 2,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          "Lista",
+                          style: ButtonStyles.sectionTitleStyleSec,
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          icon: const Icon(Icons.add),
+                          color: const Color(0xFF2B3649),
+                          onPressed:
+                              _showAddItemDialog, // Chama o diálogo para adicionar item
+                        ),
+                      ],
+                    ),
+                    // EXIBINDO LISTA DE CHECKBOXES
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _shoppingList.length,
+                      itemBuilder: (context, index) {
+                        return CheckboxListTile(
+                          value: _shoppingList[index]['checked'],
+                          onChanged: (bool? newValue) {
+                            setState(() {
+                              _shoppingList[index]['checked'] = newValue!;
+                            });
+                          },
+                          controlAffinity: ListTileControlAffinity.leading,
+                          activeColor: Colors.blueAccent,
+                          checkColor: Colors.white,
+                          title: Text(
+                            _shoppingList[index]['item'],
+                            style: TextStyle(
+                              color: _shoppingList[index]['checked']
+                                  ? Colors.green
+                                  : const Color(0xFF2B3649),
+                              decoration: _shoppingList[index]['checked']
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
